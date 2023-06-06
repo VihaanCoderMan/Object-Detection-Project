2fa3a854ad5c0586c4fa5b24b2e311cd54997a44
VihaanCoderMan <87158776+VihaanCoderMan@users.noreply.github.com>
2023-06-05 18:35:51 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,108 @@
+img = "";
+function preload() {
+   img = loadImage('bowl.webp');
+}
+
+
+function setup() {
+   canvas = createCanvas(640, 420);
+   canvas.center();
+}
+function draw() {
+   image(img, 0, 0, 640, 420)
+   fill("#FF0000")
+   text("Bowl", 45, 75)
+   noFill();
+   stroke("#FF0000")
+   rect(30, 60, 450, 350 )
+}
+
+function preload() {
+    img = loadImage('food.webp');
+ }
+ 
+ 
+ function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+ }
+ function draw() {
+    image(img, 0, 0, 640, 420)
+    fill("#FF0000")
+    text("Food", 45, 75)
+    noFill();
+    stroke("#FF0000")
+    rect(30, 60, 450, 350 )
+ }
+
+ function preload() {
+    img = loadImage('greeting_card.webp');
+ }
+ 
+ 
+ function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+ }
+ function draw() {
+    image(img, 0, 0, 640, 420)
+    fill("#FF0000")
+    text("Greeting Card", 45, 75)
+    noFill();
+    stroke("#FF0000")
+    rect(30, 60, 450, 350 )
+ }
+
+ function preload() {
+    img = loadImage('pool_stick.webp');
+ }
+ 
+ 
+ function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+ }
+ function draw() {
+    image(img, 0, 0, 640, 420)
+    fill("#FF0000")
+    text("Pool Stick", 45, 75)
+    noFill();
+    stroke("#FF0000")
+    rect(30, 60, 450, 350 )
+ }
+
+ function preload() {
+    img = loadImage('socks.webp');
+ }
+ 
+ 
+ function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+ }
+ function draw() {
+    image(img, 0, 0, 640, 420)
+    fill("#FF0000")
+    text("Socks", 45, 75)
+    noFill();
+    stroke("#FF0000")
+    rect(30, 60, 450, 350 )
+ }
+
+ function preload() {
+    img = loadImage('white_out.webp');
+ }
+ 
+ 
+ function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+ }
+ function draw() {
+    image(img, 0, 0, 640, 420)
+    fill("#FF0000")
+    text("White Out", 45, 75)
+    noFill();
+    stroke("#FF0000")
+    rect(30, 60, 450, 350 )
+ }
